8c18d8eafb55433f0a6a5d62587e8640f491e297
Pasupatirajesh <pasupatirajesh@gmail.com>
2016-08-09 15:38:08 -0500

    Update TrailerFetcher.java

--- TrailerFetcher.java
+++ TrailerFetcher.java
@@ -22,7 +22,7 @@ import javax.net.ssl.HttpsURLConnection;
 public class TrailerFetcher
 {
     public static final String TAG = "TrailerFetcher";
-    public static final String API_KEY = "0c8e96b68c5e24e2ee85490b30b0e383";
+    public static final String API_KEY = "Your API KEY";
     private Context mContext;
 
     public TrailerFetcher()
